03d0021333ac7acac7cec533316cf17ed8220875
Nikolasslim <tolstyk1234@gmail.com>
2024-01-07 21:53:02 +0100

    7 lvl kata

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function take(arr, n) {
 */
 
 ////////////////////////////////////////////
+
+/*
+function longest(s1, s2) {
+  let sort1 = [...new Set(s1.split("").sort())];
+  let sort2 = [...new Set(s2.split("").sort())];
+  let arr = [];
+  for (let i = 0; i < sort1.length; i++) {
+    arr.push(sort1[i]);
+  }
+  for (let i = 0; i < sort2.length; i++) {
+    arr.push(sort2[i]);
+  }
+  let sort3 = [...new Set(arr)];
+  console.log(sort3);
+  return sort3.sort().join("");
+}
+*/
+////////////////////////////////////////////
